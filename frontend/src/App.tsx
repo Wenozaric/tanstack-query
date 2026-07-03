2dec0f43c74a8d3753e2ff8b7d1b9d814485e03f
wenozaric <kfvnettimy@rambler.ru>
2026-07-03 20:09:17 +0300

    check invalidateQueries

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,39 @@
 import './style.css'
 import { exampleService } from './api/services'
-import { useQuery } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { type Post } from './types/types'
 
 function App() {
+
+	const queryClient = useQueryClient()
 
 	const fetchData = async () => {
 		const res = await exampleService.fetchData()
 		return res.data
 	}
 
-	const { data} = useQuery<Post[]>({
-		queryKey: ["posts"],
-		queryFn: fetchData
+	const { data, isLoading, isPending, isFetching } = useQuery<Post[]>({
+		queryKey: ['posts'],
+		queryFn: fetchData,
+		staleTime: 5000,
+		//refetchInterval: 10000,
 	})
-	
+
+	const staleData = () => {
+		queryClient.invalidateQueries({ queryKey: ['posts']})
+	}
+
 	return (
 		<div className='m-12'>
-			{data?.map((post, id) => <div key={id}>{post.name}</div>)}
+			{isLoading && <div>Loading</div>}
+			{isPending && <div>Pending</div>}
+			{isFetching && <div>Fetching</div>}
+			{data?.map((post, id) => (
+				<div key={id}>{post.name}</div>
+			))}
+			<button className='border p-4' onClick={staleData}>
+				Invalidate
+			</button>
 		</div>
 	)
 }
